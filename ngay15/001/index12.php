5dd8d47fec2aec775c66010acb1e2f92e072e35b
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-03-19 20:56:45 +0700

    121314

---
+++ ngay15/001/index12.php
@@ -0,0 +1,19 @@
+<?php
+    $domain = "en";
+    switch ($domain) {
+        case "en":
+            echo "Anh";
+            break;
+        case "vi";
+            echo "Việt Nam";
+            break;
+        case "cn";
+            echo "Trung quốc";
+            break;
+        case "jp";
+            echo "Nhật bản";
+            break;
+        default:
+            echo "không xác định";
+    }
+?>
