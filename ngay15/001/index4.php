442c29134c5bd4ddb15ad9462f5d4f6145889167
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-03-19 19:56:20 +0700

    4

---
+++ ngay15/001/index4.php
@@ -0,0 +1,33 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <pre>
+        biến trogn php
+        biến là thứ/đối tượng/gì đó lưu trữ giá trị và có thể thay đổi giá trị mà biến chứa trong quá trình chạy
+        hằng không thay đổi trong quá trình chạy
+    </pre>
+
+    <?php
+        //trong js dùng từ khoá var a; let a; const a;
+        //trong js dùng 1 trong 3 từ khoá
+
+        $b = 1;
+        $a = 5;
+
+        echo "<br>Tổng: ".($a+$b);
+        echo "<br>Hiệu: ".($a-$b);
+        echo "<br>Tích: ".($a*$b);
+        echo "<br>Thương: ".($a/$b);
+        echo "<br>Lấy dư: ".($a%$b);
+
+
+    ?>
+</body>
+</html>
