f5de08108b9d688c26bc4a8ed493394fbb0143b1
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-03-28 17:46:02 +0700

    4

--- ngay18/001/index4.php
+++ ngay18/001/index4.php
@@ -6,8 +6,8 @@ $result= preg_match_all($pattern,$subject);
 var_dump($result);
 
 
-// preg_match($pattern,$subject) trẩ về số 1 nếu $$pattern tìm thấy trong $subject
-// preg_match_all($pattern,$subject) trẩ về số lần nếu $$pattern tìm thấy trong $subject
+// preg_match($pattern,$subject) trẩ về số 1 nếu $pattern tìm thấy trong $subject
+// preg_match_all($pattern,$subject) trẩ về số lần nếu $pattern tìm thấy trong $subject
 ?>
 
 
