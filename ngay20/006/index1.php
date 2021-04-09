0c13a1c3d36491c0d6ec705fbc943339e1b8619c
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-04-09 21:12:43 +0700

    12345678

---
+++ ngay20/006/index1.php
@@ -0,0 +1,13 @@
+<?php
+//loại 1: lỗi notice (thông báo)
+// loại 2: lỗi warning (cảnh báo)
+// loại 3: lỗi fatal error (dừng chương trình tập lệnh)
+
+$cities = ["hn" => "hà nội", "dn" => "đà nẵng"];
+//if (isset($cities["abc"])) {
+//    echo $cities["abc"];
+//}
+
+echo $cities["abc"];
+
+echo "<br> chạy đến đây";
---
+++ ngay20/006/index2.php
@@ -0,0 +1,4 @@
+<?php
+echo implode(",",5);
+$cities = ["hn" => "hà nội", "dn" => "đà nẵng"];
+echo "<br>" .implode(",", $cities);
