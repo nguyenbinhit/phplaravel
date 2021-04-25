f3b6a7a9bbd86649dc5519019c9b328a77879f9c
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-04-25 17:34:52 +0700

    003

--- ngay23/003/delete.php
+++ ngay23/003/delete.php
@@ -1,10 +1,11 @@
 <?php
 
 require_once "conn.php";
-
 $id = isset($_GET["id"]) ? $_GET["id"] : 0;
 $sqlDelete = "DELETE FROM producs WHERE product_id=$id";
-$resultDelete = $connection->exec($sqlDelete);
+//echo $sqlDelete;
+$resultDelete = $conn->exec($sqlDelete);
+//var_dump($resultInsert);
 header("Location: list.php");
 exit();
-
+//
