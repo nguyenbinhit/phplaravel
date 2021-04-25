feecb057d6167f6549397ef9a77c4fcdf50f6a74
nguyenbinhit <69001423+nguyenbinhit@users.noreply.github.com>
2021-04-25 21:03:17 +0700

    111

---
+++ UsersManageProject/index.php
@@ -0,0 +1,52 @@
+<?php
+
+?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+
+    <!-- Latest compiled and minified CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+
+
+</head>
+<body>
+
+<div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Danh sách sinh viên</h1>
+
+            <div style="margin: 20px">
+                <a href="create.php" class="btn btn-info">Thêm mới sinh viên</a>
+            </div>
+            <table class="table">
+                <thead>
+                <tr>
+                    <th>User ID</th>
+                    <th>User Name</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>User Email</th>
+                    <th>User Phone</th>
+                    <th>User Password</th>
+                    <th>User Avatar</th>
+                    <th>User Birthday</th>
+                    <th>User Desc</th>
+                </tr>
+                </thead>
+                <tbody>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</div>
+
+</body>
+</html>
